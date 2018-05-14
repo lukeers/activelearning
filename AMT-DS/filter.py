2243c07d5976f85f4bc0e7a4177c784e8343b670
Nisha Pillai <nmbpillai@gmail.com>
2018-05-14 11:04:47 -0400

    Mechanical Turk Descriptions - 6k

---
+++ AMT-DS/filter.py
@@ -0,0 +1,9 @@
+import util
+import sys
+
+st = sys.argv
+fName = str(st[1])
+#fName = "Batch_3166944_batch_results.csv"
+#fName = "Batch_3169679_batch_results.csv"
+#fName = "Batch_3178122_batch_results.csv"
+util.fileringSentences(fName)
